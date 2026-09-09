011754c133b30db1c6ddc0229e43c3f191bebfce
Jason Weinzierl <weinzierljason@gmail.com>
2026-09-09 06:31:31 -0500

    style: increase linting rules and enforcement (#31)
    * chore(deps-dev): tseslint 8.69.0
    * chore(deps): run npm dedupe
    * chore: enforce linting on library
    * style: enable typed linting and fix recommended
    * style: unnecessary type assertions
    * chore: add vitest lint rules
    * chore: add rxjs lint rules
    * chore(deps-dev): eslint-plugin-prettier 5.5.6
    * chore(deps-dev): eslint 9.39.5
    * chore(deps-dev): prettier 3.9.6

--- eslint.config.js
+++ eslint.config.js
@@ -4,19 +4,30 @@ const { defineConfig } = require('eslint/config');
 const tseslint = require('typescript-eslint');
 const angular = require('angular-eslint');
 const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
+const vitest = require('@vitest/eslint-plugin');
+const rxjsX = require('eslint-plugin-rxjs-x');
 
 module.exports = defineConfig([
   {
     files: ['**/*.ts'],
     extends: [
       eslint.configs.recommended,
-      tseslint.configs.recommended,
+      tseslint.configs.recommendedTypeChecked,
       tseslint.configs.stylistic,
       angular.configs.tsRecommended,
+      rxjsX.configs.strict,
     ],
     processor: angular.processInlineTemplates,
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+      },
+    },
     rules: {
       '@angular-eslint/component-class-suffix': 'off',
+
+      'rxjs-x/no-ignored-default-value': 'off',
+      'rxjs-x/no-ignored-error': 'off',
     },
   },
   {
@@ -25,4 +36,21 @@ module.exports = defineConfig([
     rules: {},
   },
   eslintPluginPrettierRecommended,
+  {
+    files: ['**/*.spec.ts'],
+    extends: [vitest.configs.recommended],
+    settings: {
+      vitest: {
+        typecheck: true,
+      },
+    },
+    rules: {
+      '@typescript-eslint/unbound-method': 'off',
+      'vitest/unbound-method': 'error',
+
+      'vitest/require-top-level-describe': 'error',
+
+      'vitest/padding-around-all': 'error',
+    },
+  },
 ]);
